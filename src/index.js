12b401d8cd41cdbf8e275cac0e94a1848afea973
Zephyrus02 <aneeshraskar@gmail.com>
2024-04-06 18:21:05 +0530

    Analytics removed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Analytics } from "@vercel/analytics/react";
-import { inject } from "@vercel/analytics";
 
 inject();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Analytics />
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
