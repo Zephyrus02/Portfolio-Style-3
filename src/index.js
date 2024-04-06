e05b02a16244cf1ba5c2d0b51812eada67e23400
Zephyrus02 <aneeshraskar@gmail.com>
2024-04-06 18:12:23 +0530

    Added Analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Analytics />
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 reportWebVitals();
