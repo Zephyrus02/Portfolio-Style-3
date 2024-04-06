a48c9a676a37916b539ca8c9522a391adc32951d
Zephyrus02 <aneeshraskar@gmail.com>
2024-04-06 18:31:08 +0530

    Rolled back

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+inject();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 reportWebVitals();
